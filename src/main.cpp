766190eee541e5f94c93db86dfc9e93188e9e8b6
Ahmed Sherif <ahmedselfiky11@gmail.com>
2022-09-28 06:16:59 +0200

    Add Input State Machine

--- src/main.cpp
+++ src/main.cpp
@@ -14,86 +14,154 @@
 #include <vector>
 #include <memory>
 
-
-
 #include "Node.h"
 
-
-void print(const std::vector<std::string> & v){
-	for (const auto & it : v){
-	    std::cout << it << std::endl;
+void print(const std::vector<std::string> &v) {
+	for (const auto &it : v) {
+		std::cout << it << std::endl;
 	}
 }
 
-std::vector<std::string> split(const std::string &s, const char &delim) {
-    std::vector<std::string> result;
-    std::string item;
+std::vector<std::string> parse_string(const std::string &s, const char &delim) {
+	std::vector<std::string> result;
+	std::string item;
 
-    std::stringstream ss {s};
+	std::stringstream ss { s };
 
-        while (std::getline(ss, item, delim)) {
-            result.push_back (item);
-        }
+	while (std::getline(ss, item, delim)) {
+		result.push_back(item);
+	}
 
-    return result;
+	return result;
 }
 
-
 int main() {
-	enum keywords{
-		AND,
-		OR,
-		SET,
-		SIM
+	enum firstCommandKeyword_t {
+		AND_COMMAND,
+		OR_COMMAND,
+		SET_COMMAND,
+		SIM_COMMAND
 	};
 
-	std::map<std::string, keywords>  parse {
-		{"AND", AND},
-		{"OR", OR},
-		{"SET", SET},
-		{"SIM", SIM}
+	enum secondCommandKeyword_t {
+		ALL_COMMAND
 	};
 
-	std::map<std::string, keywords>::iterator itr = parse.begin();
-	std::string input, parsedInput;
+	std::map<std::string, firstCommandKeyword_t> firstCommandMap {
+		{ "AND", AND_COMMAND },
+		{ "OR", OR_COMMAND },
+		{"SET", SET_COMMAND },
+		{ "SIM", SIM_COMMAND }
+	};
+
+	std::map<std::string, secondCommandKeyword_t> secondCommandMap {
+		{ "ALL", ALL_COMMAND },
+	};
+
+	enum state_t {
+		GET_FIRST_COMMAND_STATE,
+		GET_SECOND_COMMAND_STATE,
+		GET_NODE_STATE,
+		GET_VALUE_STATE,
+		END_PARSE_STATE
+	};
+
+	auto firstCommandIterator = firstCommandMap.begin();
+	std::string inputString;
 	const char stringDelimeter = ' ';
 
-	bool isDone = false;
+	state_t state;
+
+	struct flag_t{
+			bool endParse;
+			bool startSimulation;
+			bool error;
+
+			flag_t(){
+				endParse = false;
+				startSimulation = false;
+				error = false;
+			}
+	};
+
+	flag_t flag;
 
 
 	do {
-		std::getline(std::cin, input);
-		std::vector<std::string> buffer = split(input, stringDelimeter);
+		// get user Reading
+		std::getline(std::cin, inputString);
 
-		for (const auto & i : buffer){
-			itr = parse.find(i);
+		//Store every word in a buffer
+		std::vector<std::string> buffer = parse_string(inputString, stringDelimeter);
 
-			if (itr != parse.end()) {
-				switch (itr->second) {
-					case AND: {
-						std::cout << "You Have Entered AND" << std::endl;
-						break;
+		state = GET_FIRST_COMMAND_STATE;
+
+		for (const auto &i : buffer) {
+
+			switch (state){
+				case GET_FIRST_COMMAND_STATE: {
+
+					firstCommandIterator = firstCommandMap.find(i);
+					if (firstCommandIterator != firstCommandMap.end()) {
+						switch (firstCommandIterator->second) {
+							case AND_COMMAND: {
+								std::cout << "You Have Entered AND" << std::endl;
+								//create gate pointer that points to new And gate object
+								state = GET_NODE_STATE;
+								break;
+							}
+							case OR_COMMAND: {
+								std::cout << "You Have Entered OR" << std::endl;
+								state = GET_NODE_STATE;
+								break;
+							}
+							case SET_COMMAND: {
+								std::cout << "You Have Entered SET" << std::endl;
+								state = GET_NODE_STATE;
+								break;
+							}
+							case SIM_COMMAND: {
+								std::cout << "You Have Entered SIM" << std::endl;
+								flag.startSimulation = true;
+								state = GET_FIRST_COMMAND_STATE;
+								break;
+							}
+						}
+					} else {
+						std::cout << "WRONG COMMAND" << std::endl;
+						flag.error = true;
+						state = END_PARSE_STATE;
 					}
-					case OR: {
-						std::cout << "You Have Entered OR" << std::endl;
-						break;
-					}
-					case SET: {
-						std::cout << "You Have Entered SET" << std::endl;
-						break;
-					}
-					case SIM: {
-						std::cout << "You Have Entered SIM" << std::endl;
-						isDone = true;
-						break;
-					}
+
+					break;
 				}
-			} else {
-				std::cout << "WRONG INPUT" << std::endl;
+
+				case GET_NODE_STATE: {
+
+
+					break;
+				}
+
+				case GET_SECOND_COMMAND_STATE: {
+
+					break;
+				}
+
+				case GET_VALUE_STATE: {
+
+					break;
+				}
+
+				case END_PARSE_STATE: {
+					flag.endParse = true;
+					break;
+				}
 			}
 		}
 
-	} while (isDone == false);
+		//Post-Command Process
+
+	} while (flag.endParse == false);
 
 	return 0;
 }
